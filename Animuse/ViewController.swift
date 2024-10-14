43d73435d2a6940d1b0cbb58bee5ebea73ec403f
aufar <adlan.aufar@phincon.com>
2024-10-14 11:43:37 +0700

    Initial Commit

---
+++ Animuse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animuse
+//
+//  Created by PT Phincon on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
